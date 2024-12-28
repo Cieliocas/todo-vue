b4bb3e0cc9cee633f653e7d1960a34e797f67172
Cieliocas <francielioevangelista@hotmail.com>
2024-12-28 03:15:51 -0300

    add bootstrap and implement task management UI in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <script setup>
+import { reactive } from 'vue';
 
+	const estado = reactive({
+		tarefas: [
+			{
+				titulo: 'Estudar ES6',
+				finalizada: false,
+			},
+			{
+				titulo: 'Estudar SASS',
+				finalizada: false,
+			},
+			{
+				titulo: 'Ir para a academia',
+				finalizada: true,
+			},
+		]
+	});
 </script>
 
 <template>
-
+	<div class="container">
+		<header class="p-5 mb-4 mt-4 bg-light rounded-3">
+			<h1>Minhas tarefas</h1>
+			<p>
+				Você possui <strong>7</strong> tarefas pendentes.
+			</p>
+		</header>
+		<form>
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Digite a descrição da tarefa">
+				</div>
+				<div class="col-md-2">
+					<button type="submit" class="btn btn-primary">Cadastrar</button>
+				</div>
+				<div class="col-md-2">
+					<select class="form-control">
+						<option value="todas">Todas tarefas</option>
+						<option value="pendentes">Pendentes</option>
+						<option value="finalizadas">Finalizadas</option>
+					</select>
+				</div>
+			</div>
+		</form>
+		<ul class="list-group mt-4">
+			<li class="list-group-item" v-for="tarefa in estado.tarefas">
+				<input :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+				<label class="ms-3" :for="tarefa.titulo">
+					{{ tarefa.titulo }}
+				</label>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <style scoped>
-
+	.done {
+		text-decoration: line-through;
+	}
 </style>
